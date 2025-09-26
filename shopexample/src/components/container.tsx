1cd96120ba7b297df065aedf1aacf1b4b15abaf4
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-09-27 00:10:37 +0330

    create container component

---
+++ shopexample/src/components/container.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+
+interface ContainerProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export default function Container({ children, className }: ContainerProps) {
+  return (
+    <div
+      className={
+        " max-w-[1500px] w-full my-0 mx-auto px-4 lx:px-8 lx:py-0 " + className
+      }
+    >
+      {children}
+    </div>
+  );
+}
