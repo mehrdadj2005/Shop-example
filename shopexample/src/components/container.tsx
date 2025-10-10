075a6aa25b07bf52a76e5f82e8162e03e013c2b9
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-10-10 19:31:09 +0330

    change max width container component

--- shopexample/src/components/container.tsx
+++ shopexample/src/components/container.tsx
@@ -11,7 +11,7 @@ export default function Container({ children, className }: ContainerProps) {
   return (
     <div
       className={
-        " max-w-[1500px] w-full my-0 mx-auto px-4 lx:px-8 lx:py-0 " + className
+        " !max-w-[1400px] w-full my-0 mx-auto px-4 lx:px-8 lx:py-0 " + className
       }
     >
       {children}
