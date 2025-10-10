37d7639e8e90cbceb30f3a7e04e79dc558d8b8b5
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-10-10 19:34:31 +0330

    create header types

---
+++ shopexample/src/types/header.ts
@@ -0,0 +1,15 @@
+export type HeaderData = {
+  item: {
+    logo: { src: string | undefined };
+    items: {
+      items1: string[];
+      items2: string[];
+      items3: string[];
+    };
+    images: {
+      top: string;
+      left: string;
+      bottom: string;
+    };
+  };
+};
