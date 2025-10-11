953a5a9dd41b9ec94a426a9d3ab1ad42392cc20a
mehrdadj2005 <mehrdadjokari13@gmail.com>
2025-10-11 14:20:15 +0330

    add Hero component to page.tsx

--- shopexample/src/app/layout.tsx
+++ shopexample/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Header from "@/components/layoue/header";
 import type { Metadata } from "next";
 import "./globals.css";
 
--- shopexample/src/app/page.tsx
+++ shopexample/src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/page/hero";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
