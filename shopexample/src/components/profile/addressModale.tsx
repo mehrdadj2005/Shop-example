3d2c7b273fe311c9df3c649664c255da78c5ef53
mehrdadj2005 <mehrdadjokari13@gmail.com>
2026-02-26 11:41:30 +0330

    fix: update address modal to handle address addition and editing correctly

--- shopexample/src/components/profile/addressModale.tsx
+++ shopexample/src/components/profile/addressModale.tsx
@@ -12,13 +12,13 @@ interface NewAddressProps {
   setNewAddressModal: (value: boolean) => void;
 }
 interface EditAddressProps {
-  id: number;
-  editAddressModal: boolean;
-  setEditAddressModal: (value: boolean) => void;
+  editAddressModal: string;
+  setEditAddressModal: (value: { show: boolean; text: string }) => void;
 }
 
 function NewAddress({ newAddressModal, setNewAddressModal }: NewAddressProps) {
   const [icons, setIcons] = useState<Icons>({});
+  const [address, setAddress] = useState<string>("");
 
   useEffect(() => {
     async function getData() {
@@ -27,6 +27,22 @@ function NewAddress({ newAddressModal, setNewAddressModal }: NewAddressProps) {
     }
     getData();
   }, []);
+
+  const AddAddressToLS = () => {
+    const addresses = localStorage.getItem("addresses");
+    console.log(address);
+    const allAddresses: string[] = addresses?.length
+      ? JSON.parse(addresses)
+      : [];
+    const findingAddress = allAddresses?.find((item) => item == address);
+    !findingAddress?.length
+      ? localStorage.setItem(
+          "addresses",
+          JSON.stringify([...allAddresses, address])
+        )
+      : "";
+    setNewAddressModal(!newAddressModal);
+  };
 
   return (
     <div className="w-full h-screen bg-[#00000040] absolute top-0 ring-0 left-0">
@@ -45,13 +61,17 @@ function NewAddress({ newAddressModal, setNewAddressModal }: NewAddressProps) {
           />
         </div>
         <div className="w-full px-4">
-          <Textarea className="min-h-36 max-h-36 border-2 border-primary text-stone-700" />
+          <Textarea
+            className="min-h-36 max-h-36 border-2 border-primary text-stone-700"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
         </div>
 
         <Button
           variant={"secondary"}
           className="mt-8 border-2 text-stone-500 font-medium text-lg"
-          onClick={() => setNewAddressModal(!newAddressModal)}
+          onClick={AddAddressToLS}
         >
           ADD
         </Button>
@@ -60,12 +80,12 @@ function NewAddress({ newAddressModal, setNewAddressModal }: NewAddressProps) {
   );
 }
 
-function EditAddres({
-  id,
+function EditAddress({
   editAddressModal,
   setEditAddressModal,
 }: EditAddressProps) {
   const [icons, setIcons] = useState<Icons>({});
+  const [address, setAddress] = useState<string>("");
 
   useEffect(() => {
     async function getData() {
@@ -75,6 +95,24 @@ function EditAddres({
     getData();
   }, []);
 
+  useEffect(() => {
+    setAddress(editAddressModal);
+  }, [editAddressModal]);
+
+  const cheangeAddressToLS = () => {
+    const addresses = localStorage.getItem("addresses");
+    const allAddresses: string[] = addresses?.length
+      ? JSON.parse(addresses)
+      : [];
+    allAddresses.map((item, index) => {
+      if (item == editAddressModal) {
+        allAddresses.splice(index, 1, address);
+        localStorage.setItem("addresses", JSON.stringify(allAddresses));
+      }
+    });
+    setEditAddressModal({ show: false, text: "" });
+  };
+
   return (
     <div className="w-full h-screen bg-[#00000040] absolute top-0 ring-0 left-0">
       <div className="bg-white rounded-2xl w-10/12 h-80 absolute top-1/6 left-0 right-0 mx-auto flex items-center px-2 flex-col py-4">
@@ -83,7 +121,7 @@ function EditAddres({
             EDIT ADDRESS
           </h3>
           <Image
-            onClick={() => setEditAddressModal(!editAddressModal)}
+            onClick={() => setEditAddressModal({ show: false, text: "" })}
             className="mr-2 ml-auto mb-6"
             src={icons.close}
             alt="arrow left"
@@ -93,13 +131,17 @@ function EditAddres({
         </div>
 
         <div className="w-full px-4">
-          <Textarea className="min-h-36 max-h-36 border-2 border-primary text-stone-700" />
+          <Textarea
+            className="min-h-36 max-h-36 border-2 border-primary text-stone-700"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
         </div>
 
         <Button
           variant={"secondary"}
           className="mt-8 border-2 text-stone-500 font-medium text-lg"
-          onClick={() => setEditAddressModal(!editAddressModal)}
+          onClick={cheangeAddressToLS}
         >
           OK
         </Button>
@@ -108,4 +150,4 @@ function EditAddres({
   );
 }
 
-export { EditAddres, NewAddress };
+export { EditAddress, NewAddress };
