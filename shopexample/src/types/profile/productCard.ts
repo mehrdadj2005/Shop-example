cf04c5d5be8dadf3ad53f29537e274f2a507b0e1
mehrdadj2005 <mehrdadjokari13@gmail.com>
2026-02-26 11:42:51 +0330

    feat: add ProfileProductCardType for product card representation

---
+++ shopexample/src/types/profile/productCard.ts
@@ -0,0 +1,10 @@
+export type ProfileProductCardType = {
+  id: number;
+  description: string;
+  image: string;
+  size: number;
+  color: string;
+  price: number;
+  discountedPrice?: number;
+  qty: number;
+};
